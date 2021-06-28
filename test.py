42a0ed4a4250930835528b761434d30b927d9a18
sajjad <hassanzadeh.sd@gmail.com>
2021-06-28 16:29:01 +0430

    init files

---
+++ test.py
@@ -0,0 +1,5 @@
+a = 20
+def test_test_test_test_test_test_test_test_test_test_test_test_test_test(value):
+    Name = 120
+test_test_test_test_test_test_test_test_test_test_test_test_test_test(a)
+print(a)
