bcbe370aa5181f4f2d3b8353b7a294568dac6631
sajjad <hassanzadeh.sd@gmail.com>
2021-06-28 16:45:58 +0430

    test again

--- test.py
+++ test.py
@@ -1,12 +1,7 @@
 a = 20
-
-
+b = 30
 def test_test_test_test_test_test_test_test_test_test_test_test_test_test(value):
     pass
-
-
 test_test_test_test_test_test_test_test_test_test_test_test_test_test(a)
-print(
-    "test_test_test_test_test_test_test_test_test _test_test_test_test_te sttest_test_test_test _test_test_test_test_t est_test_test_test _test_test asjflsajasl fasj lfsajlf aslf  asjdll jd"
-)
+print("test_test_test_test_test_test_test_test_test")
 print(a)
