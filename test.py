0fc67ca9ee8a323052007496dd9a586b2f2d1922
sajjad <hassanzadeh.sd@gmail.com>
2021-06-28 16:54:17 +0430

    dsad

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
 a = 20
+
+
 def test_test_test_test_test_test_test_test_test_test_test_test_test_test(value):
     pass
+
+
 test_test_test_test_test_test_test_test_test_test_test_test_test_test(a)
 print("test_test_test_test_test_test_test_test_test")
 print(a)
