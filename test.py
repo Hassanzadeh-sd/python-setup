b52476c2dcd2ca8543777e1cbcb9215e4b453596
sajjad <hassanzadeh.sd@gmail.com>
2021-06-28 16:42:55 +0430

    pre-commit test

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 a = 20
+
+
 def test_test_test_test_test_test_test_test_test_test_test_test_test_test(value):
-    Name = 120
+    pass
+
+
 test_test_test_test_test_test_test_test_test_test_test_test_test_test(a)
+print(
+    "test_test_test_test_test_test_test_test_test _test_test_test_test_te sttest_test_test_test _test_test_test_test_t est_test_test_test _test_test asjflsajasl fasj lfsajlf aslf  asjdll jd"
+)
 print(a)
